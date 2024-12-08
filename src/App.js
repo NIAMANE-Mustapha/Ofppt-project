2ef28e10257b5f9c789d83f89bacd2db60dff65e
hakim <mouatacimhakim90@gmail.com>
2024-12-08 19:55:40 +0100

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/Navbar'
-
+import Router from '../src/Components/Router'
 function App() {
   return (
-    <NavBar/>
+    <Router/>
   )
   
 }
--- src/Components/Router.jsx
+++ src/Components/Router.jsx
@@ -1,9 +1,8 @@
-import NavBar from "./Navbar";
+import NabvarLayout from "../layout/NabvarLayout";
 import React from "react";
+
 import {
   createBrowserRouter,
-  NavLink,
-  Outlet,
   RouterProvider,
 } from "react-router-dom";
 
@@ -11,7 +10,7 @@ export default function Header() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />, 
+      element: <NabvarLayout />, 
       children: [
         {
           path: "/",
@@ -22,48 +21,9 @@ export default function Header() {
   ]);
   
 
-  function Liens() {
-    return (
-      <div className="container p-2 my-2">
-        <nav className="nav bg-danger text-white d-flex justify-content-end">
-          <NavLink
-            className="nav-link"
-            to="/"
-            style={({ isActive }) => ({
-              color: isActive ? "yellow" : "white",
-            })}
-          >
-            Accueil
-          </NavLink>
-          <NavLink
-            className="nav-link"
-            to="/Produits"
-            style={({ isActive }) => ({
-              color: isActive ? "yellow" : "white",
-            })}
-          >
-            Produits
-          </NavLink>
-          <NavLink
-            className="nav-link"
-            to="/contact"
-            style={({ isActive }) => ({
-              color: isActive ? "yellow" : "white",
-            })}
-          >
-            Contact
-          </NavLink>
-        </nav>
-        <div className="container p-2">
-          <Outlet />
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="container">
-      <RouterProvider router={route} />
+      <RouterProvider router={router} />
     </div>
   );
 }
